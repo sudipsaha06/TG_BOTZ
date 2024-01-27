b70bbd657e4aa475c5f404cb8a4ff9152f611c24
sudipsaha06 <154732963+sudipsaha06@users.noreply.github.com>
2024-01-27 18:38:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Shareus.io')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shrs.link')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'nNmjBM0MTPVOkZugAxnQcNUkFQB2')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
